bd599498ffda225f3ad8577619691fa008e6b3f6
Angela Oryza <admin@Angelas-MacBook-Air.local>
2022-09-17 20:39:46 +0700

    Buat Angka 2

---
+++ main.js
@@ -0,0 +1,87 @@
+function main() {
+    var kanvas = document.getElementById("kanvas");
+    var gl = kanvas.getContext("webgl");
+
+    var vertices = [ 
+        -0.5, 0.5, //A : Kanan atas
+        0.5, 0.5,
+        0.5, 0.0,
+        0.35, -0.15,
+        -0.35, -0.15,
+        -0.35, -0.50,
+        0.35, -0.50,
+        0.5, -0.65,
+        -0.5, -0.65,
+        -0.5, -0.15,
+        -0.35, 0.0,
+        0.35, 0.0,
+        0.35, 0.35,
+        -0.35, 0.35
+//B : Bawah Tengah
+    ]; //buat array untuk segitiga, tapi ini masih di cpu
+
+    var buffer = gl.createBuffer(); 
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer); // ini define alamat gpu
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    //udah tersambung ke gpu (udah dipindah ke alam GPU)
+
+    //Vertex Shaders
+    var vertexShaderCode = 
+    `
+        attribute vec2 aPosition;
+        void main() {
+            float x = aPosition.x;
+            float y = aPosition.y;
+            gl_PointSize = 10.0;
+            gl_Position = vec4(x, y, 0.0, 1.0);
+            
+        }
+        `;
+
+    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShaderObject, vertexShaderCode);
+    gl.compileShader(vertexShaderObject); // sampai sini udah jadi .o
+    //Fragment Shaders
+    var fragmentShaderCode = `
+        void main() {
+            precision mediump float;
+            float r = 5.0;
+            float g = 1.0;
+            float b = 0.0;
+            float a = 1.0;
+            gl_FragColor = vec4(r, g, b, a);
+        }
+        `;
+
+    var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+    gl.compileShader(fragmentShaderObject); // sampai sini udah jadi .o
+
+    var shaderProgram = gl.createProgram(); //membuat sebuah mangkok
+    gl.attachShader(shaderProgram, vertexShaderObject); //semua object file tadi ditaruh ke mangkok yang sama
+    gl.attachShader(shaderProgram, fragmentShaderObject);
+    gl.linkProgram(shaderProgram); 
+    gl.useProgram(shaderProgram);
+
+    //Mengajari GPU bagaimana caranya mengoleksi
+    // nilai posisi dari ARRAY_BUFFER
+    //untuk setiap verteks yang sedang diproses
+
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    //                      index   dimensi              warna
+    gl.enableVertexAttribArray(aPosition);
+
+    gl.clearColor(0.0,  0.0,    0.0,    1.0);
+    //            Red   Green   Blue    Alpha(Opacity)
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.LINE_LOOP, 0, 14); 
+    //          jenis      data     count
+}
+
+window.onload = main;
+
+//LINE_LOOP : looping
+//LINE_STRIP : ngga balik
+//LINES : dua titik
