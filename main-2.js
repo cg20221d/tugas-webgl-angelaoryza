ba2ce5aaf0e54f48e2ef1a38e268709ca3cc9282
Angela Oryza <angelaoryza@gmail.com>
2022-11-16 05:22:53 +0700

    Animating first alphabet

--- main-2.js
+++ main-2.js
@@ -180,7 +180,141 @@ function main() {
       ];
     
   
+    var verticesW1 = [
+        -0.75, -0.20, 0.0,     0.0, 1.0, 5.0,       //A : Kanan atas
+        -0.70, -0.20, 0.0,     0.0, 1.0, 5.0,  
+        -0.5915, -0.75, 0.0,   0.0, 1.0, 5.0,  
+        -0.65, -0.75, 0.0,     0.0, 1.0, 5.0,  
 
+        -0.615, -0.75, 0.0,     0.0, 1.0, 5.0,       
+        -0.50, -0.28, 0.0,     0.0, 1.0, 5.0, 
+        -0.45, -0.28, 0.0,     0.0, 1.0, 5.0, 
+        -0.55, -0.75, 0.0,     0.0, 1.0, 5.0, 
+
+        -0.45, -0.28, 0.0,     0.0, 1.0, 5.0, 
+        -0.337, -0.75, 0.0,     0.0, 1.0, 5.0, 
+        -0.40, -0.75, 0.0,    0.0, 1.0, 5.0, 
+        -0.475, -0.40, 0.0,     0.0, 1.0, 5.0, 
+
+        -0.3585, -0.75, 0.0,     0.0, 1.0, 5.0, 
+        -0.25, -0.20, 0.0,     0.0, 1.0, 5.0, 
+        -0.20, -0.20, 0.0,     0.0, 1.0, 5.0, 
+        -0.30, -0.75, 0.0,     0.0, 1.0, 5.0, 
+    ];
+  
+    var indicesW1 = [
+  
+      0, 1, 2,      1, 2, 3,
+      2, 1, 0,      0, 1, 3,
+      3, 0, 1,      1, 3, 2,
+    
+      4, 5, 6,      5, 6, 7,
+      4, 7, 6,      4, 3, 2,
+      4, 0, 1,      7, 4, 0,
+      7, 4, 5,      5, 7, 6,
+
+      
+      8, 9, 10,     9, 10, 11,
+      10, 9, 8,     8, 9, 11,
+      11, 8, 9,     11, 10, 9,
+      10, 8, 9,     15, 10, 14,
+
+      12, 13, 14,    13, 14, 15,
+    //   14, 15, 12,     12, 13, 14,
+    //   14, 15, 12,     13, 12, 15,
+    //   15, 12, 13,     13, 15, 14,
+    // 14, 13, 12,        15, 12, 13,
+    // 14, 15, 12,         13, 12, 15,
+    // 14, 13, 12,         15, 11, 13,
+    13, 15, 11,       13, 14, 15,
+    14, 11, 15,         15, 11, 14,
+
+    
+    //   15, 14, 13,     15, 12, 13,
+
+
+    //   14,13,11,     11,13,14,
+
+
+
+
+
+
+      
+      
+    ];
+  
+    var verticesW2 = [
+        -0.75, -0.20, 0.0,      5.0, 0.0, 1.0,   
+        -0.72, -0.16, 0.0,      5.0, 0.0, 1.0,   
+        -0.67, -0.16, 0.0,      5.0, 0.0, 1.0,   
+        -0.70, -0.20, 0.0,      5.0, 0.0, 1.0,   
+
+        -0.70, -0.20, 0.0,      5.0, 0.0, 1.0,  
+        -0.67, -0.16, 0.0,      5.0, 0.0, 1.0,  
+        -0.585, -0.63, 0.0,      5.0, 0.0, 1.0,  
+        -0.60, -0.70, 0.0,      5.0, 0.0, 1.0,  
+
+        -0.50, -0.28, 0.0,      5.0, 0.0, 1.0, 
+        -0.47, -0.24, 0.0,      5.0, 0.0, 1.0, 
+        -0.42, -0.24, 0.0,      5.0, 0.0, 1.0, 
+        -0.45, -0.28,  0.0,      5.0, 0.0, 1.0, 
+
+        -0.42, -0.24,  0.0,      5.0, 0.0, 1.0, 
+        -0.45, -0.28,  0.0,      5.0, 0.0, 1.0, 
+        -0.35, -0.70,  0.0,      5.0, 0.0, 1.0, 
+        -0.3325, -0.63,  0.0,      5.0, 0.0, 1.0, 
+
+        -0.55, -0.75,  0.0,      5.0, 0.0, 1.0, 
+        -0.51, -0.75,  0.0,      5.0, 0.0, 1.0, 
+        -0.455, -0.49,  0.0,      5.0, 0.0, 1.0, 
+        -0.475, -0.40,  0.0,      5.0, 0.0, 1.0, 
+
+        -0.25, -0.20,  0.0,      5.0, 0.0, 1.0, 
+        -0.22, -0.16,  0.0,      5.0, 0.0, 1.0, 
+        -0.17, -0.16,  0.0,      5.0, 0.0, 1.0, 
+        -0.20, -0.20,  0.0,      5.0, 0.0, 1.0, 
+
+        -0.17, -0.16,  0.0,      5.0, 0.0, 1.0, 
+        -0.20, -0.20,  0.0,      5.0, 0.0, 1.0, 
+        -0.30, -0.75,  0.0,      5.0, 0.0, 1.0, 
+        -0.27, -0.71,  0.0,      5.0, 0.0, 1.0, 
+    ];
+  
+    var indicesW2 = [
+      0, 1, 2,     0, 2, 3,
+      1,2,3,        2, 3, 0,
+
+
+
+      4, 5, 6,      5, 6, 7,
+    //   6, 7, 4,      7, 4, 5,
+
+      8, 9, 10,      9, 10, 11,
+      10, 11, 8,      11, 8, 9,
+
+      12, 13, 14,      13, 14, 15,
+      14, 15, 12,      15, 12, 13,
+      14, 12, 13,      13, 12, 15,
+
+
+    //   18, 19, 16,      19, 16, 17,
+     16, 17, 18,    17, 18, 19,
+     18, 19, 16,    19, 16, 17,
+     18, 17, 16,    
+
+      20, 21, 22,      21, 22, 23,
+      22, 23, 20,      23, 20, 21,
+
+      24, 25, 26,      25, 26, 27,
+      26, 27, 24,      27, 24, 25,
+
+      24, 25, 26,      25, 26, 27,
+      26, 27, 24,      27, 24, 25,
+
+    ];
+  
+   
   
     var objects = [
       {
@@ -211,7 +345,22 @@ function main() {
         length: 20,
         type: gl.LINES,
       },
-    
+
+      {
+        name: 'W',
+        vertices: verticesW1,
+        indices: indicesW1,
+        length: 16,
+        type: gl.TRIANGLE_STRIP,
+      },
+      {
+        name: 'W',
+        vertices: verticesW2,
+        indices: indicesW2,
+        length: 28,
+        type: gl.TRIANGLE_STRIP,
+      },
+
     ]
   
   
@@ -250,16 +399,16 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
   
-
+    // Variabel lokal
     var theta = 0.0;
     var freeze = false;
     var frameWidth = 9;
-    var horizontalSpeed = 0.0022; // NRP akhir 022
+    var horizontalSpeed = 0.0022; // NRP akhir 097
     var verticalSpeed = 0.0;
     var horizontalDelta = 0.0;
     var verticalDelta = 0.0;
     var scaleDelta = 0.0;
-    var scaleSpeed = 0.05;
+    var scaleSpeed = 0.022;
     var freezeN = 0;
     var freezeO = 0;
     var thetaX = 0.0;
@@ -351,18 +500,67 @@ function main() {
       drawing(objects[i].vertices, objects[i].indices, 0, objects[i].length, objects[i].type);
     }
   
-    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 
-        6 * Float32Array.BYTES_PER_ELEMENT, 
-        0);
-    gl.enableVertexAttribArray(aPosition);
-    var aColor = gl.getAttribLocation(shaderProgram, "aColor");
-    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 
-        6 * Float32Array.BYTES_PER_ELEMENT, 
-        3 * Float32Array.BYTES_PER_ELEMENT);
-    gl.enableVertexAttribArray(aColor);
+    function onKeyPress(event) {
+      if (event.keyCode == 37) { // left arrow
+        freezeN = 1;
+      } else if (event.keyCode == 39) { // right arrow
+        freezeN = 2;
+      } else if (event.keyCode == 38) { // up arrow
+        freezeA = 1;
+      } else if (event.keyCode == 40) { // down arrow
+        freezeA = 2;
+      }
+    }
+  
+    document.addEventListener("keydown", onKeyPress, false);
+  
+    function animateW(i)  {
+      var modely = mat4.create();
+      mat4.rotateY(modely, modely, thetaY);
+  
+      if (freezeN == 1) {
+        thetaY -= 0.01;
+      } else if (freezeN == 2) {
+        thetaY += 0.01;
+      }
+  
+      var uModel = gl.getUniformLocation(shaderProgram, "uModel");
+      var uView = gl.getUniformLocation(shaderProgram, "uView");
+      var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
+      gl.uniformMatrix4fv(uModel, false, modely);
+      gl.uniformMatrix4fv(uView, false, view);
+      gl.uniformMatrix4fv(uProjection, false, perspective);
+      drawing(objects[i].vertices, objects[i].indices, 0, objects[i].length, objects[i].type);
+    }
   
 
+    function onMouseClick(event) {
+        freeze = !freeze;
+    }
+    document.addEventListener("click", onMouseClick);
+    // Papan ketuk
+    function onKeydown(event) {
+        if (event.keyCode == 32) freeze = !freeze;  // spasi
+        // Gerakan horizontal: a ke kiri, d ke kanan
+        if (event.keyCode == 65) {  // a
+            horizontalSpeed = -0.01;
+        } else if (event.keyCode == 68) {   // d
+            horizontalSpeed = 0.01;
+        }
+        // Gerakan vertikal: w ke atas, s ke bawah
+        if (event.keyCode == 87) {  // w
+            verticalSpeed = -0.01;
+        } else if (event.keyCode == 83) {   // s
+            verticalSpeed = 0.01;
+        }
+    }
+    function onKeyup(event) {
+        if (event.keyCode == 32) freeze = !freeze;
+        if (event.keyCode == 65 || event.keyCode == 68) horizontalSpeed = 0.0;
+        if (event.keyCode == 87 || event.keyCode == 83) verticalSpeed = 0.0;
+    }
+    document.addEventListener("keydown", onKeydown);
+    document.addEventListener("keyup", onKeyup);
   
     function render() {
         gl.enable(gl.DEPTH_TEST);
@@ -390,11 +588,14 @@ function main() {
         gl.uniformMatrix4fv(uModel, false, model);
         gl.uniformMatrix4fv(uView, false, view);
         gl.uniformMatrix4fv(uProjection, false, perspective);
-
-        animate2(0)
-        animate2(1)
+        // gl.drawElements(gl.LINE_LOOP, indices.length, gl.UNSIGNED_SHORT, 0);
+        animate2(0);
+        animate2(1);
         animate21(2);
         animate21(3);
+        animateW(4);
+        animateW(5);
+
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
